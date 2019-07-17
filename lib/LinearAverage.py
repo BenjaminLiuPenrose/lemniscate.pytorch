61e42c13038e61bed3b9e885f69444ad5afffd95
BenjaminLiu <imbenliu@gmail.com>
2019-07-17 16:35:37 -0700

    register_param typo

--- lib/LinearAverage.py
+++ lib/LinearAverage.py
@@ -50,7 +50,10 @@ class LinearAverage(nn.Module):
 
         self.register_buffer('params',torch.tensor([T, momentum]));
         stdv = 1. / math.sqrt(inputSize/3)
-        self.register_parameter('memory', torch.rand(outputSize, inputSize).mul_(2*stdv).add_(-stdv))
+        # self.register_parameter('memory', torch.rand(outputSize, inputSize).mul_(2*stdv).add_(-stdv))
+        self.register_parameter('memory', None)
+        self.memory = nn.Parameter(torch.rand(outputSize, inputSize).mul_(2*stdv).add_(-stdv) )
+
 
     def forward(self, x, y):
         print(self.memory.requires_grad)
