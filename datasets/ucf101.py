41f4dc5d94027670e5edb13592d4fd9f8b9196d4
BenjaminLiu <imbenliu@gmail.com>
2019-07-20 12:34:50 -0700

    fix the same bug

--- datasets/ucf101.py
+++ datasets/ucf101.py
@@ -193,13 +193,13 @@ class UCF101Instance(data.Dataset):
         if self.temporal_transform is not None:
             frame_indices = self.temporal_transform(frame_indices)
         clip = self.loader(path, frame_indices)
-        clip = torch.from_numpy(np.array(clip))
         if self.spatial_transform is not None:
             self.spatial_transform.randomize_parameters()
             clip = [self.spatial_transform(img) for img in clip]
-        clip = torch.stack(clip, 0).permute(1, 0, 2, 3)
         if self.transform is not None:
             clip = self.transform(clip)
+        clip = torch.stack(clip, 0).permute(1, 0, 2, 3)
+
 
         target = self.data[index]
         if self.target_transform is not None:
