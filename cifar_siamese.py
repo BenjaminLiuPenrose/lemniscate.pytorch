d5cc7aa1627459d49045ced78668abb981a33d6a
BenjaminLiu <imbenliu@gmail.com>
2019-07-21 00:31:01 -0700

    change train_labels to targets

--- cifar_siamese.py
+++ cifar_siamese.py
@@ -73,12 +73,12 @@ transform_test = transforms.Compose([
 
 trainset = datasets.CIFAR10Instance(root='./data', train=True, download=True, transform=transform_train)
 # trainset = datasets.CIFAR100Instance(root='./data', train=True, download=True, transform=transform_train)
-train_batch_sampler = BalancedBatchSampler(trainset.train_labels, n_classes=10, n_samples=25)
+train_batch_sampler = BalancedBatchSampler(trainset.targets, n_classes=10, n_samples=25)
 trainloader = torch.utils.data.DataLoader(trainset, batch_sampler=train_batch_sampler, shuffle=True, num_workers=2, pin_memory = True)
 
 testset = datasets.CIFAR10Instance(root='./data', train=False, download=True, transform=transform_test)
 # testset = datasets.CIFAR100Instance(root='./data', train=False, download=True, transform=transform_test)
-test_batch_sampler = BalancedBatchSampler(testset.test_labels, n_classes=10, n_samples=25)
+test_batch_sampler = BalancedBatchSampler(testset.targets, n_classes=10, n_samples=25)
 testloader = torch.utils.data.DataLoader(testset, batch_sampler=test_batch_sampler, shuffle=False, num_workers=2, pin_memory = True)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
