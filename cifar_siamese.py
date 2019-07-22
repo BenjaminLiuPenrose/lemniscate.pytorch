234a4022f8d51995abfe1507258089e67514533f
BenjaminLiu <imbenliu@gmail.com>
2019-07-22 11:40:04 -0700

    try config 2

--- cifar_siamese.py
+++ cifar_siamese.py
@@ -95,8 +95,8 @@ if args.nce_k > 0:
     assert False
     lemniscate = NCEAverage(args.low_dim, ndata, args.nce_k, args.nce_t, args.nce_m)
 else:
-    lemniscate = LinearAverage(args.low_dim, ndata, args.nce_t, args.nce_m)
-    # lemniscate = FeatureBank(args.low_dim, ndata, args.nce_t, args.nce_m)
+    # lemniscate = LinearAverage(args.low_dim, ndata, args.nce_t, args.nce_m)
+    lemniscate = FeatureBank(args.low_dim, ndata, args.nce_t, args.nce_m)
 metrics = []
 
 if device == 'cuda':
