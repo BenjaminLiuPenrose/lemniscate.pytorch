a59089b78cdcd98d49403f423cd75527c6e3640d
BenjaminLiu <imbenliu@gmail.com>
2019-07-23 11:10:10 -0700

    add

--- test.py
+++ test.py
@@ -116,7 +116,10 @@ def kNN(epoch, net, lemniscate, trainloader, testloader, K, sigma, recompute_mem
             if batch_idx == len(testloader) - 1:
                 x = retrieval
                 norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
-                print("norm of sample vector ", [n.item() for n in norm] )
+                print("norm of memory bank ", [n.item() for n in norm] )
+                x = features
+                norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
+                print("norm of feature vector ", [n.item() for n in norm] )
 
             retrieval_one_hot.resize_(batchSize * K, C).zero_()
             retrieval_one_hot.scatter_(1, retrieval.view(-1, 1), 1)
