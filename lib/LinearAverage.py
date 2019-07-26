8bbb1f0aded7827929854bdef20eff991478f216
BenjaminLiu <imbenliu@gmail.com>
2019-07-26 00:35:37 -0700

    siamese

--- lib/LinearAverage.py
+++ lib/LinearAverage.py
@@ -106,56 +106,88 @@ class LinearAverageWithWeights(nn.Module):
 
         return out
 
-
-
-class FeatureBankOp(Function):
-    @staticmethod
-    def forward(self, x, y, memory, params):
-        batchSize = x.size(0)
-        momentum = params[1].item()
-
-        # for siamese, need to comment out
-        # memory = F.normalize(memory, p = 2, dim = 1)
-
-        # inner product
-        out = x
-
-        # update the non-parametric data
-        weight_pos = memory.index_select(0, y.data.view(-1)).resize_as_(x)
-        weight_pos.mul_(momentum)
-        weight_pos.add_(torch.mul(x.data, 1-momentum))
-        w_norm = weight_pos.pow(2).sum(1, keepdim=True).pow(0.5)
-        updated_weight = weight_pos.div(w_norm)
-        memory.index_copy_(0, y, updated_weight)
-
-        self.save_for_backward(x, memory, y, params)
-
-        return out
-
-    @staticmethod
-    def backward(self, gradOutput):
-        x, memory, y, params = self.saved_tensors
-        momentum = params[1].item()
-        batchSize = gradOutput.size(0)
-
-        # update the non-parametric data
-        weight_pos = memory.index_select(0, y.data.view(-1)).resize_as_(x)
-        weight_pos.mul_(momentum)
-        weight_pos.add_(torch.mul(x.data, 1-momentum))
-        w_norm = weight_pos.pow(2).sum(1, keepdim=True).pow(0.5)
-        updated_weight = weight_pos.div(w_norm)
-        memory.index_copy_(0, y, updated_weight)
-
-        return gradOutput, None, None, None
-
-class FeatureBank(nn.Module):
+# ========================================================================================
+class LinearAverageWithoutWeights(nn.Module):
     def __init__(self, inputSize, outputSize, T = 0.07, momentum = 0.5):
-        super(FeatureBank, self).__init__()
-        stdv = 1 / math.sqrt(inputSize / 3)
-        self.register_buffer('params',torch.tensor([T, momentum]));
-        self.register_buffer('memory', torch.rand(outputSize, inputSize).mul_(2*stdv).add_(-stdv))
-        self.nLem = outputSize
+        super(LinearAverageWithWeights, self).__init__()
+        stdv = 1. / math.sqrt(inputSize/3)
+        self.weights =  nn.Parameter(
+                        torch.rand(outputSize, inputSize).mul_(2*stdv).add_(-stdv) ,
+                        requires_grad = True
+                        )
+        self.memory = nn.Parameter(F.normalize(self.weights), requires_grad = False)
+        # self.memory = F.normalize(self.memory_learnt).cuda()
+        # self.l2norm = Normalize(2)
+        self.params = nn.Parameter(torch.tensor([T, momentum]), requires_grad = False)
 
     def forward(self, x, y):
-        out = FeatureBankOp.apply(x, y, self.memory, self.params)
+        T = self.params[0].item()
+        momentum = self.params[1].item()
+
+        out = x
+        out.div_(T)
+
+        with torch.no_grad():
+            weight_pos = self.memory.index_select(0, y.data.view(-1)) #.resize_as_(x)
+            weight_pos.mul_(momentum)
+            weight_pos.add_(torch.mul(x.data, 1-momentum))
+            w_norm = weight_pos.pow(2).sum(1, keepdim=True).pow(0.5)
+            updated_weight = weight_pos.div(w_norm)
+            self.memory.index_copy_(0, y, updated_weight)
+            # self.memory = nn.Parameter(self.memory_learnt, requires_grad = False)
+
+        # loss(x, class) = -log(exp(x[class]) / (\sum_j exp(x[j]))) = -x[class] + log(\sum_j exp(x[j]))
+
         return out
+
+# class FeatureBankOp(Function):
+#     @staticmethod
+#     def forward(self, x, y, memory, params):
+#         batchSize = x.size(0)
+#         momentum = params[1].item()
+#
+#         # for siamese, need to comment out
+#         # memory = F.normalize(memory, p = 2, dim = 1)
+#
+#         # inner product
+#         out = x
+#
+#         # update the non-parametric data
+#         weight_pos = memory.index_select(0, y.data.view(-1)).resize_as_(x)
+#         weight_pos.mul_(momentum)
+#         weight_pos.add_(torch.mul(x.data, 1-momentum))
+#         w_norm = weight_pos.pow(2).sum(1, keepdim=True).pow(0.5)
+#         updated_weight = weight_pos.div(w_norm)
+#         memory.index_copy_(0, y, updated_weight)
+#
+#         self.save_for_backward(x, memory, y, params)
+#
+#         return out
+#
+#     @staticmethod
+#     def backward(self, gradOutput):
+#         x, memory, y, params = self.saved_tensors
+#         momentum = params[1].item()
+#         batchSize = gradOutput.size(0)
+#
+#         # update the non-parametric data
+#         weight_pos = memory.index_select(0, y.data.view(-1)).resize_as_(x)
+#         weight_pos.mul_(momentum)
+#         weight_pos.add_(torch.mul(x.data, 1-momentum))
+#         w_norm = weight_pos.pow(2).sum(1, keepdim=True).pow(0.5)
+#         updated_weight = weight_pos.div(w_norm)
+#         memory.index_copy_(0, y, updated_weight)
+#
+#         return gradOutput, None, None, None
+#
+# class FeatureBank(nn.Module):
+#     def __init__(self, inputSize, outputSize, T = 0.07, momentum = 0.5):
+#         super(FeatureBank, self).__init__()
+#         stdv = 1 / math.sqrt(inputSize / 3)
+#         self.register_buffer('params',torch.tensor([T, momentum]));
+#         self.register_buffer('memory', torch.rand(outputSize, inputSize).mul_(2*stdv).add_(-stdv))
+#         self.nLem = outputSize
+#
+#     def forward(self, x, y):
+#         out = FeatureBankOp.apply(x, y, self.memory, self.params)
+#         return out
