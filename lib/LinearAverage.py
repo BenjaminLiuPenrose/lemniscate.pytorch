8deba3aa1fb04ab0a4e17d6a5108b2c9437541ef
BenjaminLiu <imbenliu@gmail.com>
2019-07-25 22:36:05 -0700

    debug

--- lib/LinearAverage.py
+++ lib/LinearAverage.py
@@ -81,7 +81,7 @@ class LinearAverageWithWeights(nn.Module):
                         requires_grad = True
                         )
         # self.memory = nn.Parameter(F.normalize(self.memory_learnt), requires_grad = False)
-        self.memory = F.normalize(self.memory_learnt)
+        self.memory = F.normalize(self.memory_learnt).cuda()
         # self.l2norm = Normalize(2)
         self.params = nn.Parameter(torch.tensor([T, momentum]), requires_grad = False)
 
@@ -92,10 +92,10 @@ class LinearAverageWithWeights(nn.Module):
         weight_pos = self.memory_learnt.index_select(0, y.data.view(-1)) #.resize_as_(x)
         w_norm = weight_pos.pow(2).sum(1, keepdim=True).pow(0.5)
         updated_weight = weight_pos.div(w_norm)
-        self.memory.index_copy_(0, y.cpu(), updated_weight.cpu()).cuda()
+        self.memory.index_copy_(0, y, updated_weight)
         # self.memory = nn.Parameter(F.normalize(self.memory_learnt), requires_grad = False)
 
-        out = torch.mm(x.data, self.memory_learnt.t().cuda())
+        out = torch.mm(x.data, self.memory_learnt.t())
         out.div_(T)
 
         # loss(x, class) = -log(exp(x[class]) / (\sum_j exp(x[j]))) = -x[class] + log(\sum_j exp(x[j]))
