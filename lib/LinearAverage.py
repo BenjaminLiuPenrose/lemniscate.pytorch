4ef3487efc29eda1da764d716bd6ee900ce665d3
BenjaminLiu <imbenliu@gmail.com>
2019-08-01 12:44:43 -0700

    fix

--- lib/LinearAverage.py
+++ lib/LinearAverage.py
@@ -90,18 +90,19 @@ class LinearAverageWithWeights(nn.Module):
         T = self.params[0].item()
         momentum = self.params[1].item()
 
-        out = torch.mm(x.data, F.normalize(self.weights.t()))
-        # out = torch.mm(x.data, self.weights.t())
+        # out = torch.mm(x.data, F.normalize(self.weights.t()))
+        out = torch.mm(x.data, self.weights.t())
         out.div_(T)
 
         with torch.no_grad():
             weight_pos = self.memory.index_select(0, y.data.view(-1)) #.resize_as_(x)
             weight_pos.mul_(momentum)
-            weight_pos.add_(torch.mul(x.data, 1-momentum))
+            # weight_pos.add_(torch.mul(x.data, 1-momentum))
+            weight_pos.add_(torch.mul(self.weights.index_select(0, y.data.view(-1)), 1-momentum))
             w_norm = weight_pos.pow(2).sum(1, keepdim=True).pow(0.5)
             updated_weight = weight_pos.div(w_norm)
             self.memory.index_copy_(0, y, updated_weight)
-            # self.memory = nn.Parameter(self.memory_learnt, requires_grad = False)
+            # self.memory = nn.Parameter(self.weights, requires_grad = False)
 
         # loss(x, class) = -log(exp(x[class]) / (\sum_j exp(x[j]))) = -x[class] + log(\sum_j exp(x[j]))
 
