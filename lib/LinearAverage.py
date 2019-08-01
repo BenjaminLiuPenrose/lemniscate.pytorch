58729686e4fd637a28634ba9d1ca34e852339ed7
BenjaminLiu <imbenliu@gmail.com>
2019-08-01 16:34:04 -0700

    bu

--- lib/LinearAverage.py
+++ lib/LinearAverage.py
@@ -21,7 +21,7 @@ class LinearAverageOp(Function):
         # out = torch.mm(x.data, memory.t())
         out.div_(T) # batchSize * N
 
-        weight_pos = memory.index_select(0, y.data.view(-1)) #.resize_as_(x)
+        weight_pos = memory.index_select(0, y.cpu().data.view(-1)).resize_as_(x)
         weight_pos.mul_(momentum)
         weight_pos.add_(torch.mul(x.data, 1-momentum))
         w_norm = weight_pos.pow(2).sum(1, keepdim=True).pow(0.5)
