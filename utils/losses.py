d7382324b18f760feee0a1b25d6cdc9ef72b0e03
BenjaminLiu <imbenliu@gmail.com>
2019-08-09 16:56:32 -0700

    debug

--- utils/losses.py
+++ utils/losses.py
@@ -65,9 +65,7 @@ class OnlineContrastiveLoss(nn.Module):
         batchSize = embeddings[negative_pairs[:, 0]].size(0)
         embeddingsDim = embeddings[negative_pairs[:, 0]].size(1)
         margin = self.margin
-        print(negative_pairs[:, 0])
-        print(negative_pairs[:, 1])
-
+        print(embeddings.shape)
         # square_pred = torch.pow(embeddings, 2)
         # margin_square = torch.pow(F.relu(
         #             embeddings - margin
