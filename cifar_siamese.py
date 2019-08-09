0ad4120609f9ad1f519c1462be26dbf2b0892e0a
BenjaminLiu <imbenliu@gmail.com>
2019-08-09 14:43:38 -0700

    add

--- cifar_siamese.py
+++ cifar_siamese.py
@@ -213,9 +213,9 @@ for epoch in range(start_epoch, start_epoch+200):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/ckpt.t7')
         best_acc = acc
-        print("="*100+"saving best_acc.npy"+"="*100)
+        print("="*100+"saving best_acc_wowights.npy"+"="*100)
         X = np.append(lemniscate.memory.cpu(), np.array([trainset.targets]).T, axis = 1)
-        np.save("best_acc.npy", X)
+        np.save("best_acc_wowights.npy", X)
 
     print('best accuracy: {:.2f}'.format(best_acc*100))
 
