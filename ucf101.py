4ec172e8b72a11f2f249e162234c86f1be0a9675
BenjaminLiu <imbenliu@gmail.com>
2019-08-10 23:50:35 -0700

    debug

--- ucf101.py
+++ ucf101.py
@@ -233,7 +233,7 @@ def train(epoch):
 
 for epoch in range(start_epoch, start_epoch+100):
     train(epoch)
-    acc = kNN(epoch, net, lemniscate, trainloader, testloader, 200, args.nce_t, 0)
+    acc = kNN_ucf101(epoch, net, lemniscate, trainloader, testloader, 200, args.nce_t, 0)
 
     if acc > best_acc:
         print('Saving..')
