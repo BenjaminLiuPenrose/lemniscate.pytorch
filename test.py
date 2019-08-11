4ea09ce6b8f39d74ac3a3f8949d5c5c02523ed10
BenjaminLiu <imbenliu@gmail.com>
2019-08-10 23:04:41 -0700

    debug

--- test.py
+++ test.py
@@ -166,48 +166,48 @@ def kNN_ucf101(epoch, net, lemniscate, trainloader, testloader, K, sigma, recomp
     # print(C.cpu())
     print(C.shape)
 
-    if recompute_memory:
-        transform_bak = trainloader.dataset.transform
-        trainloader.dataset.transform = testloader.dataset.transform
-        temploader = torch.utils.data.DataLoader(trainloader.dataset, batch_size=100, shuffle=False, num_workers=1)
-        for batch_idx, (inputs, targets, indexes) in enumerate(temploader):
-            targets = targets.cuda(non_blocking=True)
-            batchSize = inputs.size(0)
-            features = net(inputs)
-            # stop w = v process to make w stand alone
-            trainFeatures[:, batch_idx*batchSize:batch_idx*batchSize+batchSize] = features.data.t()
-        trainLabels = torch.LongTensor(temploader.dataset.targets).cuda()
-        trainloader.dataset.transform = transform_bak
-
-    top1 = 0.
-    top5 = 0.
-    end = time.time()
-    with torch.no_grad():
-        retrieval_one_hot = torch.zeros(K, C).cuda()
-        print(len(testloader))
-        for batch_idx, (inputs, targets, indexes) in enumerate(testloader):
-            end = time.time()
-            targets = targets.cuda(non_blocking=True)
-            batchSize = inputs.size(0)
-            features = net(inputs)
-            net_time.update(time.time() - end)
-            end = time.time()
-
-            dist = torch.mm(features, trainFeatures)
-
-            yd, yi = dist.topk(K, dim=1, largest=True, sorted=True)
-            candidates = trainLabels.view(1,-1).expand(batchSize, -1)
-            retrieval = torch.gather(candidates, 1, yi)
-            if batch_idx == len(testloader) - 1:
-                x = retrieval
-                norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
-                print("norm of memory bank ", [n.item() for n in norm][:5] )
-                x = features
-                norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
-                print("norm of feature vector ", [n.item() for n in norm][:5] )
-
-
-            print("debug", batchSize, K )
+    # if recompute_memory:
+    #     transform_bak = trainloader.dataset.transform
+    #     trainloader.dataset.transform = testloader.dataset.transform
+    #     temploader = torch.utils.data.DataLoader(trainloader.dataset, batch_size=100, shuffle=False, num_workers=1)
+    #     for batch_idx, (inputs, targets, indexes) in enumerate(temploader):
+    #         targets = targets.cuda(non_blocking=True)
+    #         batchSize = inputs.size(0)
+    #         features = net(inputs)
+    #         # stop w = v process to make w stand alone
+    #         trainFeatures[:, batch_idx*batchSize:batch_idx*batchSize+batchSize] = features.data.t()
+    #     trainLabels = torch.LongTensor(temploader.dataset.targets).cuda()
+    #     trainloader.dataset.transform = transform_bak
+    #
+    # top1 = 0.
+    # top5 = 0.
+    # end = time.time()
+    # with torch.no_grad():
+    #     retrieval_one_hot = torch.zeros(K, C).cuda()
+    #     print(len(testloader))
+    #     for batch_idx, (inputs, targets, indexes) in enumerate(testloader):
+    #         end = time.time()
+    #         targets = targets.cuda(non_blocking=True)
+    #         batchSize = inputs.size(0)
+    #         features = net(inputs)
+    #         net_time.update(time.time() - end)
+    #         end = time.time()
+    #
+    #         dist = torch.mm(features, trainFeatures)
+    #
+    #         yd, yi = dist.topk(K, dim=1, largest=True, sorted=True)
+    #         candidates = trainLabels.view(1,-1).expand(batchSize, -1)
+    #         retrieval = torch.gather(candidates, 1, yi)
+    #         if batch_idx == len(testloader) - 1:
+    #             x = retrieval
+    #             norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
+    #             print("norm of memory bank ", [n.item() for n in norm][:5] )
+    #             x = features
+    #             norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
+    #             print("norm of feature vector ", [n.item() for n in norm][:5] )
+    #
+    #
+    #         print("debug", batchSize, K )
             # print(int(C.cpu().numpy()))
 
             # retrieval_one_hot.resize_(batchSize * K, int(C.data) ).zero_()
@@ -220,7 +220,7 @@ def kNN_ucf101(epoch, net, lemniscate, trainloader, testloader, K, sigma, recomp
             # correct = predictions.eq(targets.data.view(-1,1))
             # print("="*50, predictions)
             # print("="*50, targets)
-            cls_time.update(time.time() - end)
+            # cls_time.update(time.time() - end)
 
             # top1 = top1 + correct.narrow(1,0,1).sum().item()
             # top5 = top5 + correct.narrow(1,0,5).sum().item()
