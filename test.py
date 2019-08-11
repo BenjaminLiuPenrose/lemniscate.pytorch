c11c0541d000b130bc4ae1f7f7fdc00c052f9a69
BenjaminLiu <imbenliu@gmail.com>
2019-08-10 23:10:20 -0700

    debug

--- test.py
+++ test.py
@@ -163,80 +163,79 @@ def kNN_ucf101(epoch, net, lemniscate, trainloader, testloader, K, sigma, recomp
     else:
         trainLabels = torch.LongTensor(trainloader.dataset.targets).cuda()
     C = trainLabels.max() + 1
-    print(C.cpu())
-    print(C.shape)
-    print(C.cpu().data)
-    print(int(C.data))
+    # print(C.cpu())
+    # print(C.shape)
+    # print(C.cpu().data)
+    # print(int(C.data))
 
-    # if recompute_memory:
-    #     transform_bak = trainloader.dataset.transform
-    #     trainloader.dataset.transform = testloader.dataset.transform
-    #     temploader = torch.utils.data.DataLoader(trainloader.dataset, batch_size=100, shuffle=False, num_workers=1)
-    #     for batch_idx, (inputs, targets, indexes) in enumerate(temploader):
-    #         targets = targets.cuda(non_blocking=True)
-    #         batchSize = inputs.size(0)
-    #         features = net(inputs)
-    #         # stop w = v process to make w stand alone
-    #         trainFeatures[:, batch_idx*batchSize:batch_idx*batchSize+batchSize] = features.data.t()
-    #     trainLabels = torch.LongTensor(temploader.dataset.targets).cuda()
-    #     trainloader.dataset.transform = transform_bak
-    #
-    # top1 = 0.
-    # top5 = 0.
-    # end = time.time()
-    # with torch.no_grad():
-    #     retrieval_one_hot = torch.zeros(K, C).cuda()
-    #     print(len(testloader))
-    #     for batch_idx, (inputs, targets, indexes) in enumerate(testloader):
-    #         end = time.time()
-    #         targets = targets.cuda(non_blocking=True)
-    #         batchSize = inputs.size(0)
-    #         features = net(inputs)
-    #         net_time.update(time.time() - end)
-    #         end = time.time()
-    #
-    #         dist = torch.mm(features, trainFeatures)
-    #
-    #         yd, yi = dist.topk(K, dim=1, largest=True, sorted=True)
-    #         candidates = trainLabels.view(1,-1).expand(batchSize, -1)
-    #         retrieval = torch.gather(candidates, 1, yi)
-    #         if batch_idx == len(testloader) - 1:
-    #             x = retrieval
-    #             norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
-    #             print("norm of memory bank ", [n.item() for n in norm][:5] )
-    #             x = features
-    #             norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
-    #             print("norm of feature vector ", [n.item() for n in norm][:5] )
-    #
-    #
-    #         print("debug", batchSize, K )
-            # print(int(C.cpu().numpy()))
+    if recompute_memory:
+        transform_bak = trainloader.dataset.transform
+        trainloader.dataset.transform = testloader.dataset.transform
+        temploader = torch.utils.data.DataLoader(trainloader.dataset, batch_size=100, shuffle=False, num_workers=1)
+        for batch_idx, (inputs, targets, indexes) in enumerate(temploader):
+            targets = targets.cuda(non_blocking=True)
+            batchSize = inputs.size(0)
+            features = net(inputs)
+            # stop w = v process to make w stand alone
+            trainFeatures[:, batch_idx*batchSize:batch_idx*batchSize+batchSize] = features.data.t()
+        trainLabels = torch.LongTensor(temploader.dataset.targets).cuda()
+        trainloader.dataset.transform = transform_bak
 
-            # retrieval_one_hot.resize_(batchSize * K, int(C.data) ).zero_()
-            # retrieval_one_hot.scatter_(1, retrieval.view(-1, 1), 1)
-            # yd_transform = yd.clone().div_(sigma).exp_()
-            # probs = torch.sum(torch.mul(retrieval_one_hot.view(batchSize, -1 , C), yd_transform.view(batchSize, -1, 1)), 1)
-            # _, predictions = probs.sort(1, True)
+    top1 = 0.
+    top5 = 0.
+    end = time.time()
+    with torch.no_grad():
+        retrieval_one_hot = torch.zeros(K, C).cuda()
+        print(len(testloader))
+        for batch_idx, (inputs, targets, indexes) in enumerate(testloader):
+            end = time.time()
+            targets = targets.cuda(non_blocking=True)
+            batchSize = inputs.size(0)
+            features = net(inputs)
+            net_time.update(time.time() - end)
+            end = time.time()
 
-            # Find which predictions match the target
-            # correct = predictions.eq(targets.data.view(-1,1))
-            # print("="*50, predictions)
-            # print("="*50, targets)
-            # cls_time.update(time.time() - end)
+            dist = torch.mm(features, trainFeatures)
 
-            # top1 = top1 + correct.narrow(1,0,1).sum().item()
-            # top5 = top5 + correct.narrow(1,0,5).sum().item()
-
-            # total += targets.size(0)
-
-            # print('Test [{}/{}]\t'
-            #       'Net Time {net_time.val:.3f} ({net_time.avg:.3f})\t'
-            #       'Cls Time {cls_time.val:.3f} ({cls_time.avg:.3f})\t'
-            #       'Top1: {:.2f}  Top5: {:.2f}'.format(
-            #       total, testsize, top1*100./total, top5*100./total, net_time=net_time, cls_time=cls_time))
+            yd, yi = dist.topk(K, dim=1, largest=True, sorted=True)
+            candidates = trainLabels.view(1,-1).expand(batchSize, -1)
+            retrieval = torch.gather(candidates, 1, yi)
+            if batch_idx == len(testloader) - 1:
+                x = retrieval
+                norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
+                print("norm of memory bank ", [n.item() for n in norm][:5] )
+                x = features
+                norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
+                print("norm of feature vector ", [n.item() for n in norm][:5] )
 
 
-    # print(top1*100./(total + 1e-8), total, top1 )
-    # print(top1/(total + 1e-8))
+            print("debug", batchSize, K, int(C.data) )
+
+            retrieval_one_hot.resize_(batchSize * K, int(C.data) ).zero_()
+            retrieval_one_hot.scatter_(1, retrieval.view(-1, 1), 1)
+            yd_transform = yd.clone().div_(sigma).exp_()
+            probs = torch.sum(torch.mul(retrieval_one_hot.view(batchSize, -1 , C), yd_transform.view(batchSize, -1, 1)), 1)
+            _, predictions = probs.sort(1, True)
+
+            Find which predictions match the target
+            correct = predictions.eq(targets.data.view(-1,1))
+            print("="*50, predictions)
+            print("="*50, targets)
+            cls_time.update(time.time() - end)
+
+            top1 = top1 + correct.narrow(1,0,1).sum().item()
+            top5 = top5 + correct.narrow(1,0,5).sum().item()
+
+            total += targets.size(0)
+
+            print('Test [{}/{}]\t'
+                  'Net Time {net_time.val:.3f} ({net_time.avg:.3f})\t'
+                  'Cls Time {cls_time.val:.3f} ({cls_time.avg:.3f})\t'
+                  'Top1: {:.2f}  Top5: {:.2f}'.format(
+                  total, testsize, top1*100./total, top5*100./total, net_time=net_time, cls_time=cls_time))
+
+
+    print(top1*100./(total + 1e-8), total, top1 )
+    print(top1/(total + 1e-8))
 
     return top1/(total + 1e-8)
