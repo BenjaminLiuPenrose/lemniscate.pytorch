3c267cd86a5ce16404b4e89bc80536dc480a945e
BenjaminLiu <imbenliu@gmail.com>
2019-08-10 22:34:57 -0700

    debug

--- test.py
+++ test.py
@@ -204,7 +204,7 @@ def kNN_ucf101(epoch, net, lemniscate, trainloader, testloader, K, sigma, recomp
                 norm = x.pow(2).sum(1, keepdim = True).pow(1./2)
                 print("norm of feature vector ", [n.item() for n in norm][:5] )
 
-            print("debug", batchSize, K, C)
+            print("debug", batchSize.cpu(), K, C.cpu())
             time.sleep(30)
             retrieval_one_hot.resize_(batchSize * K, C).zero_()
             retrieval_one_hot.scatter_(1, retrieval.view(-1, 1), 1)
