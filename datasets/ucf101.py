caaef737dfac25e790c7347d492d3bcd92ff306a
BenjaminLiu <imbenliu@gmail.com>
2019-08-11 13:57:49 -0700

    debug

--- datasets/ucf101.py
+++ datasets/ucf101.py
@@ -151,7 +151,6 @@ def make_dataset(root_path, annotation_path, subset, n_samples_for_each_video,
                         range(j, min(n_frames + 1, j + sample_duration)))
                     dataset.append(sample_j)
 
-
     return dataset, targets
 
 
