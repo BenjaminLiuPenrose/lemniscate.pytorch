a0ef53501ae37900cac6343f899264ae0c36a665
BenjaminLiu <imbenliu@gmail.com>
2019-08-11 18:23:02 -0700

    debug

--- utils/n_frames_ucf101_hmdb51.py
+++ utils/n_frames_ucf101_hmdb51.py
@@ -22,7 +22,7 @@ def class_process(dir_path, class_name):
       n_frames = 0
     else:
       image_indices.sort(reverse=True)
-      n_frames = np.min([image_indices[0], 50])
+      n_frames = np.min([image_indices[0], 64])
       print(video_dir_path, n_frames)
     with open(os.path.join(video_dir_path, 'n_frames'), 'w') as dst_file:
       dst_file.write(str(n_frames))
