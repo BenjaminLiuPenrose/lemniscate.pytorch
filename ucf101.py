8d18528ca33120564dff0bb6cbac135fc075de7f
BenjaminLiu <imbenliu@gmail.com>
2019-08-14 01:51:16 -0700

    debug

--- ucf101.py
+++ ucf101.py
@@ -203,7 +203,7 @@ def train(epoch):
     end = time.time()
     for batch_idx, (inputs, targets, indexes, findexes) in enumerate(trainloader):
         data_time.update(time.time() - end)
-        st() # view targets
+        # st() # view targets
         inputs, targets, indexes, findexes = inputs.to(device), targets.to(device), indexes.to(device), findexes.to(device)
         ### modify 0813
         b, d, c, w, h = inputs.shape; inputs = inputs.view(b*d, c, w, h)
